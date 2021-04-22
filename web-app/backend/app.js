f9c35ea4d24a61d9145a53aa71fcf19b8d1fc832
DanoFecko <dano9500@gmail.com>
2021-04-22 13:50:03 +0200

    basic get and post api calls

--- web-app/backend/app.js
+++ web-app/backend/app.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
+const bodyparser = require("body-parser");
 
-app.use('/api/properties', (req, res, next) => {
+app.use(bodyparser.json);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+});
+
+app.post("/api/properties", (req, res, next) => {
+  const property = req.body;
+  console.log(property);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get('/api/properties', (req, res, next) => {
   const posts = [
     {
       id: 324234,
       price: 10000,
-      location: 'Lubotice' ,
+      location: 'Lubotice',
       owner: 42354545,
       size: 35,
       pictures: [],
@@ -16,10 +37,10 @@ app.use('/api/properties', (req, res, next) => {
     {
       id: 646456,
       price: 200000,
-      location: 'Brno' ,
+      location: 'Brno',
       owner: 42354545,
       size: 100,
-      pictures: [312312,323244],
+      pictures: [312312, 323244],
       status: true,
       type: 2
     }
--- web-app/src/services/propertyService.ts
+++ web-app/src/services/propertyService.ts
@@ -2,18 +2,20 @@ import { Property } from '../model/property';
 import { Injectable } from '@angular/core';
 import { PropertyType } from '../model/propertyType';
 import { HttpClient } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PropertyService {
 
   private properties: Property[] = [];
+  private propertiesUpdated = new Subject<Property[]>();
 
   constructor(private http: HttpClient) {
 
   }
 
   getProperties(): void {
-    this.http.get<{ message: string, properties: Property[] }>('http://loalhost:3000/api/properties')
+    this.http.get<{ message: string, properties: Property[] }>('http://localhost:3000/api/properties')
       .subscribe((data) => {
         this.properties = data.properties;
       });
@@ -21,6 +23,15 @@ export class PropertyService {
 
   addProperty(price: number, location: string, owner: number, size: number, pictures: number[], status: boolean, type: PropertyType): void {
     const property: Property = {id: 0, price, location, owner, size, pictures, status, type};
-    this.properties.push(property);
+    this.http.post<{ message: string }>('http://localhost:3000/api/properties', property)
+      .subscribe((data) => {
+        console.log(data.message);
+        this.properties.push(property);
+        this.propertiesUpdated.next([...this.properties]);
+      });
+  }
+
+  getPropertyUpdateListener(): Observable<Property[]> {
+    return this.propertiesUpdated.asObservable();
   }
 }
